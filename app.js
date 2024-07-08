9218bee8583a2773ca9102331c3dd4dd2c2198e1
camalzadeelvin <camalzadeelvin@STHQ012D-01>
2024-07-08 10:52:10 +0400

    done

--- app.js
+++ app.js
@@ -6,20 +6,19 @@ const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
-const path = require('path');
+const path = require("path");
 
 // 1) Middlewares
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log("Hello from the middleware");
-  next();
-});
-
+// app.use((req, res, next) => {
+//   console.log("Hello from the middleware");
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -30,6 +29,15 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter);
 
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this Server!`,
+  });
+});
+
+
 
 //3 Start Server
 
