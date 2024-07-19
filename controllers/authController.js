cfe6adff9b6b8652f0c4963dde032e779553f2b8
camalzadeelvin <camalzadeelvin@STHQ012D-01>
2024-07-19 10:15:35 +0400

    updateUserpass task

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const AppError = require("./../utils/appError");
 const { promisify } = require("util");
 const sendEmail = require("./../utils/email");
+const crypto = require("crypto");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -162,4 +163,45 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on token
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  // 2) If token has not expired , and there is user , set the new password
+
+  if (!user) {
+    return next(new AppError("Token is invalid or has expired"));
+  }
+  // 3) Update changedPasswordAt property for the user
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordChangedAt = Date.now();
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+
+  await user.save();
+
+  // 4)Log the user in , send JWT
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collections
+  // 2)Check if Posted current password is correct
+  // 3) If so ,update password
+  // 4) Log user in , send JWT
+});
